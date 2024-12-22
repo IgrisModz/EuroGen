85b654426626fb6311563995b8d79eb69108de94
IgrisModz <kirito59320@gmail.com>
2024-12-22 18:31:34 +0100

    Add models for draw and statistics

---
+++ EuroGen/Models/Draw.cs
@@ -0,0 +1,39 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace EuroGen.Models;
+
+public class Draw
+{
+    [Name("boule_1")]
+    [Index(0)]
+    public int FirstNumber { get; set; }
+
+    [Name("boule_2")]
+    [Index(1)]
+    public int SecondNumber { get; set; }
+
+    [Name("boule_3")]
+    [Index(2)]
+    public int ThirdNumber { get; set; }
+
+    [Name("boule_4")]
+    [Index(3)]
+    public int FourthNumber { get; set; }
+
+    [Name("boule_5")]
+    [Index(4)]
+    public int FifthNumber { get; set; }
+
+    [Name("etoile_1")]
+    [Index(5)]
+    public int FirstStar { get; set; }
+
+    [Name("etoile_2")]
+    [Index(6)]
+    public int SecondStar { get; set; }
+
+    [Name("date_de_tirage")]
+    [Index(7)]
+    [Format("dd/MM/yyyy", "dd/MM/yy", "yyyyMMdd")]
+    public DateTime DrawDate { get; set; }
+}
---
+++ EuroGen/Models/Stats.cs
@@ -0,0 +1,14 @@
+﻿namespace EuroGen.Models;
+
+public class Stats
+{
+    public int Id { get; set; }
+
+    public int Number { get; set; }
+
+    public int NumberOfOutput { get; set; }
+
+    public required string PercentOfOutput { get; set; }
+
+    public required string LastRelease { get; set; }
+}
