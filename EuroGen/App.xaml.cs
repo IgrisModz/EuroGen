afa4837d45fad2295182e4239f7edca071e3cea4
IgrisModz <kirito59320@gmail.com>
2024-12-22 18:20:50 +0100

    Change Window Size parameters for Desktop App

--- EuroGen/App.xaml.cs
+++ EuroGen/App.xaml.cs
@@ -1,12 +1,27 @@
-﻿namespace EuroGen
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
+﻿namespace EuroGen;
 
-            MainPage = new MainPage();
-        }
+public partial class App : Application
+{
+    public const int Width = 1200;
+    public const int Height = 724;
+
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        var window = base.CreateWindow(activationState);
+        window.Width = Width;
+        window.Height = Height;
+        window.MaximumHeight = 960;
+        window.MaximumWidth = 1200;
+        window.MinimumHeight = 724;
+        window.MinimumWidth = 500;
+
+        return window;
     }
 }
