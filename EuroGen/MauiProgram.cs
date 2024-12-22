f4ca16165018f49fce35a6e4a3dec19a6f54b30d
IgrisModz <kirito59320@gmail.com>
2024-12-22 18:37:49 +0100

    Add Localization for multi languages

--- EuroGen/MauiProgram.cs
+++ EuroGen/MauiProgram.cs
@@ -8,64 +8,67 @@ using Windows.Graphics;
 using CommunityToolkit.Maui;
 using Microsoft.Extensions.Logging;
 using MudBlazor.Services;
+using EuroGen.Services;
 
-namespace EuroGen
+namespace EuroGen;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .UseMauiCommunityToolkit()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                });
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
 
 #if WINDOWS
-            builder.ConfigureLifecycleEvents(events =>
+        builder.ConfigureLifecycleEvents(events =>
+        {
+            events.AddWindows(windows =>
             {
-                events.AddWindows(windows =>
+                windows.OnWindowCreated(window =>
                 {
-                    windows.OnWindowCreated(window =>
+                    // Obtenir la fenêtre native
+                    var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                    var appWindow = AppWindow.GetFromWindowId(Win32Interop.GetWindowIdFromWindow(hwnd));
+
+                    if (appWindow != null)
                     {
-                        // Obtenir la fenêtre native
-                        var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
-                        var appWindow = AppWindow.GetFromWindowId(Win32Interop.GetWindowIdFromWindow(hwnd));
-
-                        if (appWindow != null)
+                        // Désactiver le redimensionnement
+                        switch (appWindow.Presenter)
                         {
-                            // Désactiver le redimensionnement
-                            switch (appWindow.Presenter)
-                            {
-                                case OverlappedPresenter overlappedPresenter:
-                                    overlappedPresenter.IsMaximizable = false;
-                                    break;
-                            }
-
-                            // Centrer la fenêtre
-                            var displayArea = DisplayArea.GetFromWindowId(appWindow.Id, DisplayAreaFallback.Primary);
-                            var centerX = (displayArea.WorkArea.Width - appWindow.Size.Width) / 2;
-                            var centerY = (displayArea.WorkArea.Height - appWindow.Size.Height) / 2;
-                            appWindow.Move(new PointInt32(centerX, centerY));
+                            case OverlappedPresenter overlappedPresenter:
+                                overlappedPresenter.IsMaximizable = false;
+                                break;
                         }
-                    });
+
+                        // Centrer la fenêtre
+                        var displayArea = DisplayArea.GetFromWindowId(appWindow.Id, DisplayAreaFallback.Primary);
+                        var centerX = (displayArea.WorkArea.Width - appWindow.Size.Width) / 2;
+                        var centerY = (displayArea.WorkArea.Height - appWindow.Size.Height) / 2;
+                        appWindow.Move(new PointInt32(centerX, centerY));
+                    }
                 });
             });
+        });
 #endif
 
-            builder.Services.AddMauiBlazorWebView();
+        builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+        builder.Services.AddSingleton<LocalizationService>();
 
-            builder.Services.AddMudServices();
+        builder.Services.AddMauiBlazorWebView();
+
+        builder.Services.AddMudServices();
 
 #if DEBUG
-            builder.Services.AddBlazorWebViewDeveloperTools();
-            builder.Logging.AddDebug();
+        builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Logging.AddDebug();
 #endif
 
-            return builder.Build();
-        }
+        return builder.Build();
     }
 }
---
+++ EuroGen/Services/LocalizationService.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Extensions.Localization;
+using System.Globalization;
+using System.Reflection;
+
+namespace EuroGen.Services
+{
+    public class LocalizationService
+    {
+        private const string LanguageKey = "AppLanguage";
+        private readonly IStringLocalizer _localizer;
+
+        private string _language = "";
+
+        public string Language
+        {
+            get => _language;
+            set
+            {
+                _language = value;
+                SaveLanguagePreference(value);
+                UpdateLanguage();
+            }
+        }
+
+        public Action? LanguageChanged;
+
+        public string this[string key] => _localizer[key];
+
+        public LocalizationService(IStringLocalizerFactory factory)
+        {
+            var type = typeof(Resources.Strings);
+            var assemblyName = new AssemblyName(type.GetTypeInfo().Assembly.FullName!);
+            _localizer = factory.Create(type.Name, assemblyName.Name!);
+            LoadLanguagePreference();
+        }
+
+        public static void SaveLanguagePreference(string language)
+        {
+            Preferences.Set(LanguageKey, language);
+            SetCulture(language);
+        }
+
+        // Charger la préférence de langue depuis les preferences
+        private void LoadLanguagePreference()
+        {
+            if (Preferences.ContainsKey(LanguageKey))
+            {
+                var savedLanguage = Preferences.Get(LanguageKey, "en"); // Valeur par défaut "en"
+                Language = savedLanguage;
+            }
+            else
+            {
+                string currentCulture = CultureInfo.CurrentCulture.TwoLetterISOLanguageName;
+                string language = currentCulture switch
+                {
+                    "ca" or "de" or "es" or "en" or "fr" or "ga" or "gv" or "it" or "lb" or "nl" or "pt" => currentCulture,
+                    _ => "en",
+                };
+                Language = language;
+            }
+
+            UpdateLanguage();
+        }
+
+        private void UpdateLanguage()
+        {
+            // On passe un booléen pour indiquer si le thème est sombre ou non
+            LanguageChanged?.Invoke();
+        }
+
+        public static void SetCulture(string language)
+        {
+            var currentCulture = new CultureInfo(language);
+            CultureInfo.DefaultThreadCurrentCulture = currentCulture;
+            CultureInfo.DefaultThreadCurrentUICulture = currentCulture;
+        }
+
+        public string GetString(string key)
+        {
+            return _localizer[key];
+        }
+    }
+}
