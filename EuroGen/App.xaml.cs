149061d69bbf76490555d1132f73d95e595ffe54
IgrisModz <kirito59320@gmail.com>
2024-12-22 17:43:48 +0100

    Ajoutez des fichiers projet.

---
+++ EuroGen/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EuroGen
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ EuroGen/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EuroGen
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
