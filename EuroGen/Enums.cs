c76b739a8bede60a66c623c5349ed30b3c02b0c7
IgrisModz <kirito59320@gmail.com>
2024-12-25 17:15:09 +0100

    Add global enums

---
+++ EuroGen/Enums.cs
@@ -0,0 +1,15 @@
+﻿namespace EuroGen;
+
+public enum CalculDrawType
+{
+    TotalDraw,
+    TotalNumber,
+    TotalDrawByNumber,
+    TotalNumberByNumber,
+}
+
+public enum CalculStatsType
+{
+    TotalDraw,
+    TotalNumber,
+}
