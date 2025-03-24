0952865fca22d26920df4806c28492af18959a10
IgrisModz <kirito59320@gmail.com>
2025-03-24 15:30:33 +0100

    Add navigation bar color for android

--- EuroGen/Platforms/Android/MainActivity.cs
+++ EuroGen/Platforms/Android/MainActivity.cs
@@ -1,11 +1,37 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using MApp = Microsoft.Maui.Controls;
 
 namespace EuroGen
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetNavigationBarColor();
+
+            MApp.Application.Current!.RequestedThemeChanged += (sender, args) =>
+            {
+                SetNavigationBarColor();
+            };
+        }
+
+        private void SetNavigationBarColor()
+        {
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                var lightColor = (Color?)MApp.Application.Current?.Resources["PageBackgroundColor"];
+                var darkColor = (Color?)MApp.Application.Current?.Resources["PageBackgroundColor2"];
+
+                var currentTheme = MApp.Application.Current?.RequestedTheme;
+                var color = currentTheme == AppTheme.Dark ? darkColor : lightColor;
+
+                Window?.SetNavigationBarColor(Android.Graphics.Color.ParseColor(color?.ToHex()!));
+            }
+        }
     }
 }
