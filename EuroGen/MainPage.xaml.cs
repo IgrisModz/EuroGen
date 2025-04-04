dac3a2c5d2beb48d95c89086bf9e38ae550a9c1b
IgrisModz <kirito59320@gmail.com>
2025-04-04 17:28:50 +0200

    Fix statusbarcolor don't change on themerequestedchanged

--- EuroGen/MainPage.xaml.cs
+++ EuroGen/MainPage.xaml.cs
@@ -1,10 +1,51 @@
-﻿namespace EuroGen
+﻿using CommunityToolkit.Maui.Behaviors;
+
+namespace EuroGen;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    public MainPage()
     {
-        public MainPage()
+        InitializeComponent();
+
+#if ANDROID || IOS
+        SetStatusBarColor();
+
+        Application.Current!.RequestedThemeChanged += (sender, args) =>
         {
-            InitializeComponent();
+            SetStatusBarColor();
+        };
+#endif
+    }
+
+    public void SetStatusBarColor()
+    {
+#if ANDROID || IOS
+#if IOS
+        if (!UIKit.UIDevice.CurrentDevice.CheckSystemVersion(11, 0))
+        {
+            return;
         }
+#elif ANDROID
+        if (Android.OS.Build.VERSION.SdkInt < Android.OS.BuildVersionCodes.Lollipop)
+        {
+            return;
+        }
+#endif
+        var existingBehavior = Behaviors.OfType<StatusBarBehavior>().FirstOrDefault();
+        if (existingBehavior != null)
+        {
+            Behaviors.Remove(existingBehavior);
+        }
+
+        var newBehavior = new StatusBarBehavior
+        {
+            StatusBarColor = Application.Current!.RequestedTheme == AppTheme.Dark
+                ? (Color)Application.Current.Resources["DarkAssetColor"]
+                : (Color)Application.Current.Resources["LightAssetColor"]
+        };
+
+        Behaviors.Add(newBehavior);
+#endif
     }
 }
