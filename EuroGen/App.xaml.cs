4ba7767120a68e8296d3bf6b61843508728ff615
IgrisModz <kirito59320@gmail.com>
2025-04-16 18:21:04 +0200

    Change window size parameters on Windows

--- EuroGen/App.xaml.cs
+++ EuroGen/App.xaml.cs
@@ -2,26 +2,26 @@
 
 public partial class App : Application
 {
-    public const int Width = 1200;
-    public const int Height = 724;
+    public const int Width = 620;
+    public const int Height = 680;
 
     public App()
     {
         InitializeComponent();
-
-        MainPage = new MainPage();
     }
 
     protected override Window CreateWindow(IActivationState? activationState)
     {
-        var window = base.CreateWindow(activationState);
-        window.Title = "EuroGen";
-        window.Width = Width;
-        window.Height = Height;
-        window.MaximumHeight = 960;
-        window.MaximumWidth = 1200;
-        window.MinimumHeight = 724;
-        window.MinimumWidth = 500;
+        var window = new Window(new MainPage())
+        {
+            Title = "EuroGen",
+            Width = Width,
+            Height = Height,
+            MaximumHeight = 960,
+            MaximumWidth = Width,
+            MinimumHeight = Height,
+            MinimumWidth = 420
+        };
 
         return window;
     }
