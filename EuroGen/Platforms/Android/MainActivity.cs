ee437c1b5438e72baf38cf601d2793ae78719b0c
IgrisModz <kirito59320@gmail.com>
2025-04-16 18:08:50 +0200

    Change  the way to change navigation bar color for android

--- EuroGen/Platforms/Android/MainActivity.cs
+++ EuroGen/Platforms/Android/MainActivity.cs
@@ -2,36 +2,14 @@
 using Android.Content.PM;
 using Android.OS;
 using MApp = Microsoft.Maui.Controls;
+using Microsoft.Maui.ApplicationModel;
+using Microsoft.Maui;
+using AndroidX.Core.View;
 
 namespace EuroGen
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
-        protected override void OnCreate(Bundle? savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
-
-            SetNavigationBarColor();
-
-            MApp.Application.Current!.RequestedThemeChanged += (sender, args) =>
-            {
-                SetNavigationBarColor();
-            };
-        }
-
-        private void SetNavigationBarColor()
-        {
-            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
-            {
-                var lightColor = (Color?)MApp.Application.Current?.Resources["LightAssetColor"];
-                var darkColor = (Color?)MApp.Application.Current?.Resources["DarkAssetColor"];
-
-                var currentTheme = MApp.Application.Current?.RequestedTheme;
-                var color = currentTheme == AppTheme.Dark ? darkColor : lightColor;
-
-                Window?.SetNavigationBarColor(Android.Graphics.Color.ParseColor(color?.ToHex()!));
-            }
-        }
     }
 }
