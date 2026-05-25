29afc7380f1977df5d06d470ce108ebd7a1cdf0b
IgrisModz <kirito59320@gmail.com>
2026-05-25 18:04:03 +0200

    Adds BlazorWebView and simplifies status bar management
    Adds a BlazorWebView component to MainPage.xaml to host the Blazor application. Removes manual status bar color management from MainPage.xaml.cs, which is now handled by StatusBarBehavior in the XAML.

--- EuroGen/MainPage.xaml.cs
+++ EuroGen/MainPage.xaml.cs
@@ -1,38 +1,9 @@
-﻿using System.Runtime.Versioning;
-
-namespace EuroGen;
+﻿namespace EuroGen;
 
 public partial class MainPage : ContentPage
 {
     public MainPage()
     {
         InitializeComponent();
-
-        if (OperatingSystem.IsAndroid() || OperatingSystem.IsIOS())
-        {
-            SetStatusBarColor();
-
-            Application.Current!.RequestedThemeChanged += (sender, args) =>
-            {
-                SetStatusBarColor();
-            };
-        }
-    }
-
-    [SupportedOSPlatform("android21.0")]
-    [SupportedOSPlatform("ios11.0")]
-    public void SetStatusBarColor()
-    {
-        if (OperatingSystem.IsAndroidVersionAtLeast(21) || OperatingSystem.IsIOSVersionAtLeast(11, 0))
-        {
-
-            var lightColor = (Color?)Application.Current?.Resources["LightAssetColor"];
-            var darkColor = (Color?)Application.Current?.Resources["DarkAssetColor"];
-
-            var currentTheme = Application.Current?.RequestedTheme;
-            var isDarkMode = currentTheme == AppTheme.Dark;
-
-            CommunityToolkit.Maui.Core.Platform.StatusBar.SetColor(isDarkMode ? darkColor : lightColor);
-        }
     }
 }
