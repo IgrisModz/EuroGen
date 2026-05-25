9d5aa47ae7f7456ed2535ed9fcd4928fa5101a8c
IgrisModz <kirito59320@gmail.com>
2026-05-25 17:39:50 +0200

    Removal of specific WinUI code and simplification of the app
    Removal of the Windows window management block in MauiProgram.cs (centering and resizing). Simplification of the App.xaml.cs structure for greater clarity.

--- EuroGen/MauiProgram.cs
+++ EuroGen/MauiProgram.cs
@@ -27,38 +27,6 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
             });
 
-#if WINDOWS
-        builder.ConfigureLifecycleEvents(events =>
-        {
-            events.AddWindows(windows =>
-            {
-                windows.OnWindowCreated(window =>
-                {
-                    // Obtenir la fenêtre native
-                    var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
-                    var appWindow = AppWindow.GetFromWindowId(Win32Interop.GetWindowIdFromWindow(hwnd));
-
-                    if (appWindow != null)
-                    {
-                        // Désactiver le redimensionnement
-                        switch (appWindow.Presenter)
-                        {
-                            case OverlappedPresenter overlappedPresenter:
-                                overlappedPresenter.IsMaximizable = false;
-                                break;
-                        }
-
-                        // Centrer la fenêtre
-                        var displayArea = DisplayArea.GetFromWindowId(appWindow.Id, DisplayAreaFallback.Primary);
-                        var centerX = (displayArea.WorkArea.Width - appWindow.Size.Width) / 2;
-                        var centerY = (displayArea.WorkArea.Height - appWindow.Size.Height) / 2;
-                        appWindow.Move(new PointInt32(centerX, centerY));
-                    }
-                });
-            });
-        });
-#endif
-
         builder.Services.AddDbContext<AppDbContext>();
 
         builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
